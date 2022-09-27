6c293a12205cd55e14e7c9f7a0f1daa49f1a9dcc
AlexAnishin <sinka_6799@mail.ru>
2022-09-27 21:34:31 +0300

    molegame

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,15 +1,18 @@
-function killer() {
-    let x = document.getElementById("dead").textContent;
-    let y = document.getElementById("lost").textContent;
-    let moleInLink = hole.className.includes('hole_has-mole').textContent;
-    if (moleInLink) {
-        x++;
-    } else {
-        y++;
+function killer(hole) {
+    return () => {
+        let x = document.getElementById("dead").textContent;
+        let y = document.getElementById("lost").textContent;
+        let moleInLink = hole.className.includes('hole_has-mole');
+        if (moleInLink) {
+            x++;
+        } else {
+            y++;
+        }
+        document.getElementById("dead").textContent = x;
+        document.getElementById("lost").textContent = y;
     }
 }
-let index = 1;
-let hole = document.getElementById(`hole${index}`);
-
-
-hole.onclick = killer;
+for (let i = 1; i <= 9; i++) {
+    let hole = document.getElementById(`hole${i}`);
+    hole.onclick = killer(hole);
+}
